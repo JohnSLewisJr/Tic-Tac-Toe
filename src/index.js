666fe5f9a5652bd24aa31e91cb39923874028b59
redjohn95 <jacklewis50@yahoo.com>
2021-04-03 19:31:01 -0500

    final code update

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import './index.css';
 
 /*  Note1: In React, its conventional to use on[Event] names for propsl which represent events and handle[Event] for the methods which handle the events.
     Note2: 2 approaches to changing data: 1: Mutation = directly changing data values. 2: Replace data with a new copy which has desired changes (allows for "time-traveling", easier change detection and re-rendering)
-*/
+    Note3: its strongly recommended to assign proper keys whenever you build dynamic lists
+    */
 
 // Square is a custom component that renders a single button
 //class Square extends React.Component {
@@ -31,44 +32,16 @@ function Square(props) {
 
 // Board is a custom component that renders 9 squares
 class Board extends React.Component {
-    constructor(props) { /* this constructor initializes the state */
-        super(props);   /* In JS classes, you need to ALWAYS call super when defining the constructor of a subclass. All React component classes that have a constructor should start with a super(props) call. */
-        this.state = {
-            squares: Array(9).fill(null), // Board's initial state is set to contain an array of 9 nulls corresponding to the 9 squares
-            xIsNext: true, //makes X the default first move
-        };
-    }
-
-    handleClick(i) {
-        const squares = this.state.squares.slice(); //.slice() creates a copy of the squares array to modify instead of modifying the existing array
-        if(calculateWinner(squares) || squares[i]) {
-            return;
-        }
-        squares[i] = this.state.xIsNext ? 'X' : '0';
-        this.setState({
-            squares: squares,
-            xIsNext: !this.state.xIsNext,   
-        });
-    }
-
     renderSquare(i) { /* renderSquare is a method */
         return (
             <Square 
-                value={this.state.squares[i]} //value prop is a function passed to Square to share value
-                onClick={() => this.handleClick(i)} //onClick prop is a function that Square can call when clicked
+                value={this.props.squares[i]} //value prop is a function passed to Square to share value
+                onClick={() => this.props.onClick(i)} //onClick prop is a function that Square can call when clicked
             />
         ); 
     }
 
     render() { //call calculateWinner(squares) in the Board's render function to check if a player has won. If player has won, display text either "Winner: X" or "Winner: O"
-        const winner = calculateWinner(this.state.squares);
-        let status;
-        if (winner) {
-            status = 'Winner: ' + winner;
-        } else {
-            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-        }
-
         return (
             <div>
                 <div className="status">{status}</div>
@@ -94,15 +67,75 @@ class Board extends React.Component {
 
 // Game is a custom component that renders a board 
 class Game extends React.Component {
+    constructor(props) {  /* this constructor initializes the state */
+        super(props); /* In JS classes, you need to ALWAYS call super when defining the constructor of a subclass. All React component classes that have a constructor should start with a super(props) call. */
+        this.state = {
+            history: [{
+                squares: Array(9).fill(null),
+            }],
+            stepNumber: 0,
+            xIsNext: true, 
+        }
+    }
+
+    handleClick(i) {
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
+        const current = history[history.length - 1];
+        const squares = current.squares.slice(); //.slice() creates a copy of the squares array to modify instead of modifying the existing array
+        if(calculateWinner(squares) || squares[i]) {
+            return;
+        }
+        squares[i] = this.state.xIsNext ? 'X' : '0';
+        this.setState({
+            history: history.concat([{
+                squares: squares,
+            }]),
+            stepNumber: history.length,
+            xIsNext: !this.state.xIsNext,   
+        });
+    }
+
+    jumpTo(step) {
+        this.setState({
+            stepNumber: step,
+            xIsNext: (step % 2) === 0,
+        });
+    }
+    
     render() {
+        const history = this.state.history;
+        const current = history[this.state.stepNumber];
+        const winner = calculateWinner(current.squares);
+
+        const moves = history.map((step, move) => {
+            const desc = move ?
+                'Go to move #' + move :
+                'Go to game start';
+            return (
+                <li key={move}>
+                    <button onClick={() => this.jumpTo(move)}>{desc}</button>
+                </li>
+            );
+        });
+
+        let status;
+        if (winner) {
+            status = 'Winner: ' + winner;
+        } else {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
+
         return (
             <div className="game">
                 <div className="game-board">
-                    <Board />
+                    <Board
+                        squares={current.squares}
+                        onClick={(i) => this.handleClick(i)}
+                    />
                 </div>
                 <div className="game-info">
-                    <div>{/* status */}</div>
-                    <ol>{/* TODO */}</ol>
+                    <div>{status}</div>
+                    <ol>{moves}</ol>
                 </div>
             </div>
         );
